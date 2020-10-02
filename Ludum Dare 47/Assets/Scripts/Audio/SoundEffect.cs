07002ade82d8267a5a1dae9d718378e7857d496b
MarcusOtter <github@otterstrom.dev>
2020-10-03 01:18:56 +0200

    Add sound effect player

---
+++ Ludum Dare 47/Assets/Scripts/Audio/SoundEffect.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Sound Effect")]
+public class SoundEffect : ScriptableObject
+{
+    [Header("Volume")]
+    [SerializeField, Range(0, 1)] private float _baseVolume = 0.5f;
+    [SerializeField] private bool _hasRandomVolume;
+    [SerializeField, Range(0, 1)] private float _maxVolume;
+    [SerializeField, Range(0, 1)] private float _minVolume;
+
+    [Header("Audio clips")]
+    [SerializeField] private bool _hasRandomClip;
+    [SerializeField] private AudioClip[] _audioClips;
+
+    [Header("Pitch")]
+    [SerializeField, Range(0, 4)] private float _basePitch = 1f;
+    [SerializeField] private bool _hasRandomPitch;
+    [SerializeField, Range(0, 1)] private float _maxPitch;
+    [SerializeField, Range(0, 1)] private float _minPitch;
+
+    public AudioClip GetClip()
+    {
+        if (_audioClips.Length == 0) { return null; }
+
+        return _hasRandomClip
+            ? _audioClips[Random.Range(0, _audioClips.Length)]
+            : _audioClips[0];
+    }
+
+    public float GetVolume()
+    {
+        return _hasRandomVolume
+            ? Random.Range(_minVolume, _maxVolume)
+            : _baseVolume;
+    }
+
+    public float GetPitch()
+    {
+        return _hasRandomPitch
+            ? Random.Range(_minPitch, _maxPitch)
+            : _basePitch;
+    }
+}
---
+++ Ludum Dare 47/Assets/Scripts/Audio/SoundEffectPlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class SoundEffectPlayer : MonoBehaviour
+{
+    internal static SoundEffectPlayer Instance { get; private set; }
+
+    private void OnEnable()
+    {
+        AssertSingleton();
+    }
+
+    public void Play(SoundEffect soundEffect, float delay = 0f)
+    {
+        var clip = soundEffect.GetClip();
+        var volume = soundEffect.GetVolume();
+        var pitch = soundEffect.GetPitch();
+
+        var gameObjectToSpawn = new GameObject($"{clip.name} player", typeof(AudioSource));
+        var spawnedAudioSource = Instantiate(gameObjectToSpawn, transform.position, Quaternion.identity).GetComponent<AudioSource>();
+
+        spawnedAudioSource.clip = clip;
+        spawnedAudioSource.volume = volume;
+        spawnedAudioSource.pitch = pitch;
+
+        spawnedAudioSource.Play();
+
+        Destroy(spawnedAudioSource.gameObject, delay + clip.length + 0.1f);
+    }
+
+    private void AssertSingleton()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
